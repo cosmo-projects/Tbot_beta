68773dd9524ea288db632b6896631f7a41acffe3
Astra1000 <astra.offic@gmail.com>
2025-05-12 03:11:52 +0300

    Update del.py

--- bot_cmd/del.py
+++ bot_cmd/del.py
@@ -7,12 +7,10 @@ command = "del"
 async def handler(client: Client, message: Message, args: str, settings: dict):
     if not message.reply_to_message:
         await message.reply("""
-         ╭───⋞⋅ SYSTEM INFO
-         │
+         ╭───⋞⚙️ SYSTEM INFO ⚙️
          ├─▶❗Произошла ошибка! 
          ├─▶ ❌ Я что сам должен догадатся че удалить? 
-         │
-         ╰───⋞⋅ 💙Powered by Cosmo
+         ╰───⋞🌌 Powered by Cosmo 🌌
         """)
         return
 
@@ -22,17 +20,15 @@ async def handler(client: Client, message: Message, args: str, settings: dict):
             message.reply_to_message.id
         )
         await message.reply("""
-        ╭───⋞⋅ SYSTEM INFO
+        ╭───⋞⚙️ SYSTEM INFO ⚙️
         ├─▶ 🗑️ Сообщение удалено !
-        ╰───⋞⋅ 💙Powered by Cosmo
+        ╰───⋞🌌 Powered by Cosmo 🌌
         """)
     except Exception as e:
         await message.reply(f"""
-        ╭───⋞⋅ SYSTEM INFO
-        │
+        ╭───⋞⚙️ SYSTEM INFO ⚙️
         ├─▶❗Произошла критическая ошибка! 
         ├─▶ ⚠️ Ошибка: {str(e)}")
-        │
-        ╰───⋞⋅ 💙Powered by Cosmo 
+        ╰───⋞🌌 Powered by Cosmo 🌌 
         """) 
         
